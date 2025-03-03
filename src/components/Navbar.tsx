33407fc6fbdf3a014d9000377b924a9166c3923a
KEM-CONSOLATION <consolationlotachi@gmail.com>
2025-03-03 06:56:33 +0100

    added projects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -76,107 +76,109 @@ const Navbar = () => {
   };
 
   return (
-    <div className="max-w-7xl mx-[10px] md:mx-auto">
-      <div className="sticky top-0 z-50 flex justify-center md:justify-between items-center py-[20px] px-4 lg:px-8 bg-[#0A192F]">
-        <Link href="/">
-          <Image
-            src={Logo}
-            alt="logo"
-            className="max-w-[50px] sm:max-w-[80px] w-full"
-          />
-        </Link>
-
-        <div className="hidden lg:block">
-          <ul className="text-[16px] text-[#E7E8EA] list-none font-ClashDisplaySemiBold flex items-center gap-[24px]">
-            <li
-              className={getDesktopNavItemClasses("about")}
-              onClick={() => handleSmoothScroll("about")}
-            >
-              <span className="font-ClashDisplaySemiBold text-[#64FFDA]">
-                01.
-              </span>{" "}
-              About
-            </li>
-            <li
-              className={getDesktopNavItemClasses("projects")}
-              onClick={() => handleSmoothScroll("projects")}
-            >
-              <span className="font-ClashDisplaySemiBold text-[#64FFDA]">
-                02.
-              </span>{" "}
-              Projects
-            </li>
-            <li
-              className={getDesktopNavItemClasses("contact")}
-              onClick={() => handleSmoothScroll("contact")}
-            >
-              <span className="font-ClashDisplaySemiBold text-[#64FFDA]">
-                03.
-              </span>{" "}
-              Contact
-            </li>
-          </ul>
-        </div>
-
-        {activeSection !== "resume" && (
-          <Link href="/resume">
-            <button className="hidden md:block px-[16px] sm:px-[32px] py-[12px] text-[16px] font-ClashDisplaySemiBold border border-[#64FFDA] rounded-[8px] text-[#64FFDA]">
-              View my resume
-            </button>
+    <div className="w-full sticky top-0 z-50 bg-[#0A192F]">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-center md:justify-between items-center py-[20px] px-4 lg:px-8">
+          <Link href="/">
+            <Image
+              src={Logo}
+              alt="logo"
+              className="max-w-[50px] sm:max-w-[80px] w-full"
+            />
           </Link>
-        )}
 
-        <div className="fixed right-0 bottom-0 w-full lg:hidden border-t bg-[#0A192F] border-[#E7E8EA]">
-          <ul className="text-[12px] !text-[#48484A] list-none font-ClashDisplaySemiBold flex items-center justify-between px-4 py-2">
-            <li
-              className={getMobileNavItemClasses("about")}
-              onClick={() => handleSmoothScroll("about")}
-            >
-              <Image
-                src="/assets/home-01.svg"
-                alt="about"
-                width={20}
-                height={20}
-              />
-              <span>About</span>
-            </li>
-            <li
-              className={getMobileNavItemClasses("projects")}
-              onClick={() => handleSmoothScroll("projects")}
-            >
-              <Image
-                src="/assets/share.svg"
-                alt="Features"
-                width={24}
-                height={24}
-              />
-              <span>Projects</span>
-            </li>
+          <div className="hidden lg:block">
+            <ul className="text-[16px] text-[#E7E8EA] list-none font-ClashDisplaySemiBold flex items-center gap-[24px]">
+              <li
+                className={getDesktopNavItemClasses("about")}
+                onClick={() => handleSmoothScroll("about")}
+              >
+                <span className="font-ClashDisplaySemiBold text-[#64FFDA]">
+                  01.
+                </span>{" "}
+                About
+              </li>
+              <li
+                className={getDesktopNavItemClasses("projects")}
+                onClick={() => handleSmoothScroll("projects")}
+              >
+                <span className="font-ClashDisplaySemiBold text-[#64FFDA]">
+                  02.
+                </span>{" "}
+                Projects
+              </li>
+              <li
+                className={getDesktopNavItemClasses("contact")}
+                onClick={() => handleSmoothScroll("contact")}
+              >
+                <span className="font-ClashDisplaySemiBold text-[#64FFDA]">
+                  03.
+                </span>{" "}
+                Contact
+              </li>
+            </ul>
+          </div>
 
-            <li
-              className={getMobileNavItemClasses("contact")}
-              onClick={() => handleSmoothScroll("contact")}
-            >
-              <Image
-                src="/assets/call-calling_.svg"
-                alt="Contact"
-                width={24}
-                height={24}
-              />
-              <span>Contact</span>
-            </li>
+          {activeSection !== "resume" && (
             <Link href="/resume">
-              <li className={getMobileNavItemClasses("resume")}>
+              <button className="hidden md:block px-[16px] sm:px-[32px] py-[12px] text-[16px] font-ClashDisplaySemiBold border border-[#64FFDA] rounded-[8px] text-[#64FFDA]">
+                View my resume
+              </button>
+            </Link>
+          )}
+
+          <div className="fixed right-0 bottom-0 w-full lg:hidden border-t bg-[#0A192F] border-[#E7E8EA] z-50">
+            <ul className="text-[12px] !text-[#48484A] list-none font-ClashDisplaySemiBold flex items-center justify-between px-4 py-2">
+              <li
+                className={getMobileNavItemClasses("about")}
+                onClick={() => handleSmoothScroll("about")}
+              >
                 <Image
-                  src="/assets/document-text.svg"
-                  alt="FAQ"
+                  src="/assets/home-01.svg"
+                  alt="about"
+                  width={20}
+                  height={20}
+                />
+                <span>About</span>
+              </li>
+              <li
+                className={getMobileNavItemClasses("projects")}
+                onClick={() => handleSmoothScroll("projects")}
+              >
+                <Image
+                  src="/assets/share.svg"
+                  alt="Features"
                   width={24}
                   height={24}
                 />
-                <span>Resume</span>
+                <span>Projects</span>
               </li>
-            </Link>
-          </ul>
+
+              <li
+                className={getMobileNavItemClasses("contact")}
+                onClick={() => handleSmoothScroll("contact")}
+              >
+                <Image
+                  src="/assets/call-calling_.svg"
+                  alt="Contact"
+                  width={24}
+                  height={24}
+                />
+                <span>Contact</span>
+              </li>
+              <Link href="/resume">
+                <li className={getMobileNavItemClasses("resume")}>
+                  <Image
+                    src="/assets/document-text.svg"
+                    alt="FAQ"
+                    width={24}
+                    height={24}
+                  />
+                  <span>Resume</span>
+                </li>
+              </Link>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
